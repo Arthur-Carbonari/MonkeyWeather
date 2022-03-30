567c109e872498ddbb707a52be4d1c3864faa4c3
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2022-03-30 07:12:48 +0100

    Add eventListener and anon function for userInputF

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const chatBox = document.getElementById("chatBox");
 const userInputField = document.getElementById("userInput");
+
+userInputField.addEventListener("keydown", function (e) {
+    if (e.code == "Enter") {
+        let userInput = getUserInput();
+        processUserInput(userInput);
+    }
+});
