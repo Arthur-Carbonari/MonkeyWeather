caae5a36f3ddf99eec2beff632d516e6f41455ae
MertBekar <mert.bekar@student.griffith.ie>
2022-03-30 04:50:12 +0100

    second test for getUserInput implemented

--- test/test.js
+++ test/test.js
@@ -9,3 +9,11 @@ QUnit.test("getUserInput1", assert => {
 
     assert.equal(weatherApp.getUserInput(userInputField), "first_test");
 })
+
+QUnit.test("getUserInput2", assert => {
+    let userInputField = {
+        value: "not_equal_test"
+    };
+
+    assert.notEqual(weatherApp.getUserInput(userInputField), "is_it_equal");
+})
